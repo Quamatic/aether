c6315a4bbedfb54ceab4611f7cbf363a97a978fe
Quamatic <Quamatic@users.noreply.github.com>
2024-11-18 21:57:00 +0000

    deploy: f6620074ef46e3712b25b04de2b8d0f02361edda

---
+++ assets/js/814f3328.4138af88.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Release","permalink":"/aether/blog/2024/11/16/release","unlisted":false,"date":"2024-11-16T00:00:00.000Z"}]}')}}]);
